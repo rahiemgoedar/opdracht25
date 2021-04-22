7cf232860844b220188c399b7fe622825ef97a16
rahiemgoedar <79698303+rahiemgoedar@users.noreply.github.com>
2021-04-22 15:09:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//Checking if a number is big
+const biggerThenHundred = function (number) { //this functions does something: it checks.
+   const checkNumber = number > 100; 
+   return checkNumber;
+}
+const result = biggerThenHundred (101);
+console.log(result); 
+
+//Bouncer at a club
+/* const brendaBouncer = function (nr1, nr2) {
+const sum = nr1 && nr2; {
+return "(nr1 => 400)" ;
+}
+return "come inside";
+};
+const result = brendaBouncer(399, 5);
+console.log(result); */
+
+//Calculate the average
